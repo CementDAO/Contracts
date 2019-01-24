26b59c2ea813ba9132a616edb5adcf0ce2555e11
Alberto Cuesta <alberto@techhq.io>
2019-01-24 17:00:21 +0000

    Deploy Fixidity library

--- migrations/2_deploy_mixr_and_setlib.js
+++ migrations/2_deploy_mixr_and_setlib.js
@@ -1,8 +1,11 @@
 const MIXR = artifacts.require('./MIXR.sol');
 const AddressSetLib = artifacts.require('./AddressSetLib.sol');
+const FixidityLib = artifacts.require('./fixidity/FixidityLib.sol');
 
 module.exports = (deployer) => {
     deployer.deploy(AddressSetLib);
     deployer.link(AddressSetLib, MIXR);
+    deployer.deploy(FixidityLib);
+    deployer.link(FixidityLib, MIXR);
     deployer.deploy(MIXR);
 };
