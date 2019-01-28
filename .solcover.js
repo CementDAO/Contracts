fe5d10ff5b146a4e47fa10b84d05eaaa68ef8a66
Bernardo Vieira <bernardo5@live.com.pt>
2019-01-28 13:26:54 +0000

    updated files to be verified on test coverage

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,9 @@ module.exports = {
   skipFiles: [
     "contracts/Migrations.sol",
     "test/SampleERC20.sol",
-    "test/SampleERC721.sol"
+    "test/SampleERC721.sol",
+    "fixidity/FixidityLib.sol",
+    "fixidity/ExponentLib.sol",
+    "fixidity/LogarithmLib.sol"
   ]
 };
