cd5c79f2680c06b669d6e12c820c121b766196f5
Bernardo Vieira <bernardo5@live.com.pt>
2019-01-30 09:11:51 +0000

    completing fixidity library tests - WIP

---
+++ contracts/mocks/LogarithmLibMock.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+import "../fixidity/LogarithmLib.sol";
+
+/**
+ * @dev 
+ */
+
+contract LogarithmLibMock {
+
+    function log_e(int256 value) public pure returns (int256) {
+        return LogarithmLib.log_e(value);
+    }
+
+    function log_any(int256 base, int256 v) public pure returns (int256) {
+        return LogarithmLib.log_any(base, v);
+    }
+}
--- test/libraries/FixidityLibMock.js
+++ test/libraries/FixidityLibMock.js
@@ -10,8 +10,52 @@ contract('FixidityLibMock', () => {
     });
 
     it('newFromInt256', async () => {
-        const balance = await fixidityLibMock.newFromInt256(1);
-        console.log(balance);
+        const result = await fixidityLibMock.newFromInt256(1);
+        console.log(result.toString(10));
     });
 
+    it('newFromInt256Fraction', async () => {
+        const result = await fixidityLibMock.newFromInt256Fraction(1, 10);
+        console.log(result.toString(10));
+    });
+
+    it('integer', async () => {
+        const result = await fixidityLibMock.integer(new BigNumber(2).pow(36));
+        console.log(result.toString(10));
+    });
+
+    it('integer', async () => {
+        const result = await fixidityLibMock.fractional(new BigNumber(2).pow(36));
+        console.log(result.toString(10));
+    });
+
+    it('abs', async () => {
+        const result = await fixidityLibMock.abs(-5);
+        console.log(result.toString(10));
+    });
+
+    it('multiply', async () => {
+        const result = await fixidityLibMock.multiply(2, 3);
+        console.log(result.toString(10));
+    });
+
+    it('reciprocal', async () => {
+        const result = await fixidityLibMock.reciprocal(2);
+        console.log(result.toString(10));
+    });
+
+    it('divide', async () => {
+        const result = await fixidityLibMock.divide(4, 2);
+        console.log(result.toString(10));
+    });
+
+    it('add', async () => {
+        const result = await fixidityLibMock.add(4, 2);
+        console.log(result.toString(10));
+    });
+
+    it('subtract', async () => {
+        const result = await fixidityLibMock.subtract(4, 2);
+        console.log(result.toString(10));
+    });
 });
