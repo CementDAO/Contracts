aaf4f423bdb36ceb7e7515503b5fdf36ecbabefa
Bernardo Vieira <bernardo5@live.com.pt>
2019-01-30 14:45:54 +0000

    fixidity lib tests - fractional

--- test/libraries/FixidityLibMock.js
+++ test/libraries/FixidityLibMock.js
@@ -1,7 +1,10 @@
 const FixidityLibMock = artifacts.require('./FixidityLibMock.sol');
 const BigNumber = require('bignumber.js');
+const chai = require('chai');
 
 const { itShouldThrow } = require('../utils');
+// use default BigNumber
+chai.use(require('chai-bignumber')()).should();
 
 
 contract('FixidityLibMock', () => {
@@ -157,9 +160,35 @@ contract('FixidityLibMock', () => {
         });
     });
 
-    it('fractional', async () => {
-        const result = await fixidityLibMock.fractional(new BigNumber(2).pow(36));
-        console.log(result.toString(36));
+    describe('fractional', () => {
+        it('fractional(0)', async () => {
+            const result = new BigNumber(await fixidityLibMock.fractional(0));
+            result.should.be.bignumber.equal(0);
+        });
+        it('fractional(fixed_1())', async () => {
+            const result = new BigNumber(
+                await fixidityLibMock.fractional(fixed_1.toString(10)),
+            );
+            result.should.be.bignumber.equal(0);
+        });
+        it('fractional(fixed_1()-1)', async () => {
+            const result = new BigNumber(
+                await fixidityLibMock.fractional(fixed_1.minus(1).toString(10)),
+            );
+            result.should.be.bignumber.equal(new BigNumber(10).pow(36).minus(1));
+        });
+        it('fractional(-fixed_1())', async () => {
+            const result = new BigNumber(
+                await fixidityLibMock.fractional(fixed_1.multipliedBy(-1).toString(10)),
+            );
+            result.should.be.bignumber.equal(0);
+        });
+        it('fractional(-fixed_1()+1)', async () => {
+            const result = new BigNumber(
+                await fixidityLibMock.fractional(fixed_1.multipliedBy(-1).plus(1).toString(10)),
+            );
+            result.should.be.bignumber.equal(new BigNumber(10).pow(36).minus(1).multipliedBy(-1));
+        });
     });
 
     it('abs', async () => {
