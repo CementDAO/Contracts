f3ebfac0319a2e05a98ce6371363f9f37828dec4
Bernardo Vieira <bernardo5@live.com.pt>
2019-02-04 19:06:35 +0000

    add parameteres to erc20 test

--- contracts/test/SampleOtherERC20.sol
+++ contracts/test/SampleOtherERC20.sol
@@ -1,25 +1,26 @@
 pragma solidity ^0.5.0;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 
 /**
  * @dev The SampleERC20 contract isn't part of the business logic
  * for CementDAO. It is a fixture that is only used during tests.
  */
-contract SampleOtherERC20 is ERC20 {
-    string public constant name = "SampleOtherERC20";
-    string public constant symbol = "NEO";
-    uint8 public constant decimals = 18;
-
-    uint256 public constant INITIAL_SUPPLY = 525 * (10 ** uint256(decimals));
+contract SampleOtherERC20 is ERC20, ERC20Detailed {
 
     /**
     * @dev Constructor that gives _owner all of existing tokens.
+    * @param _owner the user receiving tokens
+    * @param supply the amount of tokens in wei
+    * @param decimals the ERC20 decimals
     */
-    constructor(address _owner) public {
-        _mint(_owner, INITIAL_SUPPLY);
-        //approve(_owner, INITIAL_SUPPLY);
+    constructor(address _owner, uint256 supply, uint8 decimals)
+        public
+        ERC20Detailed("SampleOtherERC20", "TIR", decimals)
+    {
+        _mint(_owner, supply);
     }
 
 }
