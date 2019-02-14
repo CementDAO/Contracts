7e57e57717c38e1811a64e822c0c1c34bb56717c
Bernardo Vieira <bernardo5@live.com.pt>
2019-02-14 17:47:23 +0000

    improved MIXR tests

--- test/MIXR.js
+++ test/MIXR.js
@@ -10,11 +10,181 @@ const { itShouldThrow, tokenNumber } = require('./utils');
 chai.use(require('chai-bignumber')()).should();
 
 
+/**
+ * Method to test deposit functionality
+ * @param {BigNumber} tokens amount in tokens to be redeem
+ * @param {String} user user address doing redemption
+ * @param {String} walletFees wallet address were fees are being sent
+ * @param {Object} someERC20 erc20 contract
+ * @param {Integer} someERC20Decimals erc20 decimals
+ * @param {Object} mixr mixr contract
+ * @param {Integer} mixrDecimals mixr decimals
+ */
+const depositTest = async (
+    tokens, user, walletFees, someERC20, someERC20Decimals, mixr, mixrDecimals) => {
+    /**
+     * get previous balances
+     */
+    const previousERC20Balance = new BigNumber(
+        await someERC20.balanceOf(user),
+    );
+    const previousMixrBalance = new BigNumber(await mixr.balanceOf(user));
+    /**
+     * define amounts
+     */
+    const tokensToDeposit = tokenNumber(someERC20Decimals, tokens);
+    const MIXToMint = new BigNumber(10).pow(mixrDecimals).multipliedBy(tokens);
+    /**
+     * The deposit fee should be 0.1 MIX
+     */
+    const depositFee = new BigNumber(10).pow(23).toString(10);
+    /**
+     * approve and deposit
+     */
+    await mixr.approve(mixr.address, MIXToMint.toString(10), {
+        from: user,
+    });
+    await someERC20.approve(mixr.address, tokensToDeposit.toString(10), {
+        from: user,
+    });
+    await mixr.depositToken(someERC20.address, tokensToDeposit.toString(10), {
+        from: user,
+    });
+    /**
+     * asserts - verify balances
+     */
+    // User spends the stablecoin
+    new BigNumber(await someERC20.balanceOf(user)).should.be.bignumber.equal(
+        previousERC20Balance.minus(tokensToDeposit),
+    );
+    // User receives the MIX minus the fee
+    new BigNumber(await mixr.balanceOf(user)).should.be.bignumber.equal(
+        previousMixrBalance.plus(MIXToMint.minus(depositFee)),
+    );
+    // The stakeholder account should get the fees
+    new BigNumber(await mixr.balanceOf(walletFees))
+        .should.be.bignumber.equal(depositFee);
+
+    // Since basket was empty, it should be exactly equal to the deposit
+    tokensToDeposit.should.be.bignumber.equal(
+        new BigNumber(await someERC20.balanceOf(mixr.address)),
+    );
+};
+/**
+ * Method to test redemption functionality
+ * @param {BigNumber} tokens amount in tokens to be redeem
+ * @param {String} user user address doing redemption
+ * @param {String} walletFees wallet address were fees are being sent
+ * @param {Object} someERC20 erc20 contract
+ * @param {Integer} someERC20Decimals erc20 decimals
+ * @param {Object} mixr mixr contract
+ * @param {Integer} mixrDecimals mixr decimals
+ */
+const redemptionTest = async (
+    tokens, user, walletFees, someERC20, someERC20Decimals, mixr, mixrDecimals) => {
+    /**
+     * variables sets
+     */
+    const previousERC20Balance = new BigNumber(await someERC20.balanceOf(user));
+    const previousMixrBalance = new BigNumber(await mixr.balanceOf(user));
+    const previousMixrERC20Balance = new BigNumber(await someERC20.balanceOf(mixr.address));
+    const previousWalletFeeBalance = new BigNumber(await mixr.balanceOf(walletFees));
+    const oneToken = new BigNumber(10).pow(someERC20Decimals).multipliedBy(tokens);
+    const oneMIXR = new BigNumber(10).pow(mixrDecimals).multipliedBy(tokens);
+
+    /**
+     * estimate fees to authorize transactions
+     */
+    const feeInBasketWei = new BigNumber(
+        await mixr.transactionFee(
+            someERC20.address,
+            oneToken.toString(10),
+            await mixr.REDEMPTION(),
+        ),
+    );
+    /**
+     * approve and deposit
+     */
+    const amountInBasketWei = new BigNumber(
+        await mixr.convertTokensAmount(
+            someERC20.address,
+            mixr.address,
+            oneToken.toString(10),
+        ),
+    );
+    const totalApprove = amountInBasketWei.plus(feeInBasketWei);
+    await mixr.approve(
+        mixr.address,
+        totalApprove.toString(10),
+        {
+            from: user,
+        },
+    );
+    /**
+     * redeem
+     */
+    await mixr.redeemMIXR(
+        someERC20.address,
+        amountInBasketWei.toString(10),
+        {
+            from: user,
+        },
+    );
+    /**
+     * asserts
+     */
+    const withoutFeeInBasketWei = amountInBasketWei.minus(feeInBasketWei);
+    const withoutFeeInTokenWei = new BigNumber(
+        await mixr.convertTokensAmount(
+            mixr.address,
+            someERC20.address,
+            withoutFeeInBasketWei.toString(10),
+        ),
+    );
+    const feeInTokenWei = new BigNumber(
+        await mixr.convertTokensAmount(
+            mixr.address,
+            someERC20.address,
+            feeInBasketWei.toString(10),
+        ),
+    );
+    /**
+     * if depositing 1 token and the fee is 0.1
+     * the new ERC20 user's balance should have more 0.9 tokens
+     */
+    new BigNumber(
+        await someERC20.balanceOf(user),
+    ).should.be.bignumber.equal(
+        previousERC20Balance.plus(withoutFeeInTokenWei),
+    );
+    /**
+     * since the user redeem one mix, should have one less
+     */
+    new BigNumber(await mixr.balanceOf(user))
+        .should.be.bignumber.equal(
+            previousMixrBalance.minus(oneMIXR),
+        );
+    /**
+     * same happen to mixr balance in erc20
+     * but we subtracted the fee, because it was sent
+     * to the stakeholder wallet in erc20 token
+     */
+    new BigNumber(
+        await someERC20.balanceOf(mixr.address),
+    ).should.be.bignumber.equal(
+        previousMixrERC20Balance.minus(oneToken.minus(feeInTokenWei)),
+    );
+    // The stakeholder account should get the fees
+    new BigNumber(await mixr.balanceOf(walletFees))
+        .should.be.bignumber.equal(previousWalletFeeBalance.plus(feeInBasketWei));
+};
+
 contract('MIXR', (accounts) => {
     let mixr;
     let fixidityLibMock;
     let someERC20;
     let someERC721;
+    const defaultAmountOfTokens = 100;
     const mixrDecimals = 24;
     const someERC20Decimals = 18;
     const owner = accounts[0];
@@ -38,7 +208,7 @@ contract('MIXR', (accounts) => {
     // These tests rely on another test to have changed the fixtures (ERC20 approval).
     // If the tests order is changed, or if these tests are ran in isolation they will fail.
     describe('deposit functionality', () => {
-        beforeEach(async () => {
+        const beforeEachMethod = async () => {
             /**
              * deploy mixr and sample erc20
              */
@@ -102,8 +272,11 @@ contract('MIXR', (accounts) => {
              */
             const mixrBalance = new BigNumber(await mixr.totalSupply());
             assert.equal(mixrBalance.comparedTo(new BigNumber(0)), 0, 'should be 0.');
-        });
+        };
+
         describe('actions that should fail', () => {
+            beforeEach(beforeEachMethod);
+
             afterEach(async () => {
                 const mixrBalance = new BigNumber(await mixr.totalSupply());
                 assert.equal(mixrBalance.comparedTo(new BigNumber(0)), 0, 'should be 0.');
@@ -168,54 +341,13 @@ contract('MIXR', (accounts) => {
                 'The given token isn\'t listed as accepted.',
             );
         });
-        describe('depositToken', () => {
-            it('depositToken(50)', async () => {
-                /**
-                 * get previous balances
-                 */
-                const previousERC20Balance = new BigNumber(
-                    await someERC20.balanceOf(user),
-                );
-                const previousMixrBalance = new BigNumber(await mixr.balanceOf(user));
-                /**
-                 * define amounts
-                 */
-                const tokensToDeposit = tokenNumber(18, 50);
-                const MIXToMint = new BigNumber(10 ** 24).multipliedBy(50);
-                /**
-                 * The deposit fee should be 0.1 MIX
-                 */
-                const depositFee = new BigNumber(10).pow(23).toString(10);
-                /**
-                 * approve and deposit
-                 */
-                await mixr.approve(mixr.address, MIXToMint.toString(10), {
-                    from: user,
-                });
-                await someERC20.approve(mixr.address, tokensToDeposit.toString(10), {
-                    from: user,
-                });
-                await mixr.depositToken(someERC20.address, tokensToDeposit.toString(10), {
-                    from: user,
-                });
-                /**
-                 * asserts - verify balances
-                 */
-                // User spends the stablecoin
-                new BigNumber(await someERC20.balanceOf(user)).should.be.bignumber.equal(
-                    previousERC20Balance.minus(tokensToDeposit),
-                );
-                // User receives the MIX minus the fee
-                new BigNumber(await mixr.balanceOf(user)).should.be.bignumber.equal(
-                    previousMixrBalance.plus(MIXToMint.minus(depositFee)),
-                );
-                // The stakeholder account should get the fees
-                new BigNumber(await mixr.balanceOf(walletFees))
-                    .should.be.bignumber.equal(depositFee);
 
-                // Since basket was empty, it should be exactly equal to the deposit
-                tokensToDeposit.should.be.bignumber.equal(
-                    new BigNumber(await someERC20.balanceOf(mixr.address)),
+        describe('depositToken', () => {
+            beforeEach(beforeEachMethod);
+
+            it('depositToken(50)', async () => {
+                depositTest(
+                    50, user, walletFees, someERC20, someERC20Decimals, mixr, mixrDecimals,
                 );
             });
         });
@@ -224,7 +356,7 @@ contract('MIXR', (accounts) => {
     // These tests rely on another test to have changed the fixtures (ERC20 approval).
     // If the tests order is changed, or if these tests are ran in isolation they will fail.
     describe('redemption functionality', () => {
-        before(async () => {
+        const beforeEachMethod = async () => {
             /**
              * deploy mixr and sample erc20
              */
@@ -234,7 +366,7 @@ contract('MIXR', (accounts) => {
             });
             someERC20 = await SampleERC20.new(
                 governor,
-                tokenNumber(someERC20Decimals, 100),
+                tokenNumber(someERC20Decimals, defaultAmountOfTokens),
                 someERC20Decimals,
             );
             /**
@@ -275,7 +407,7 @@ contract('MIXR', (accounts) => {
              */
             await someERC20.transfer(
                 user,
-                tokenNumber(someERC20Decimals, 100),
+                tokenNumber(someERC20Decimals, defaultAmountOfTokens),
                 { from: governor },
             );
             /**
@@ -286,7 +418,8 @@ contract('MIXR', (accounts) => {
              * send tokens to mixr contract, so we can redeem
              * in order to use redeemMIXR method, we should deposit first
              */
-            const tokensToTransfer = new BigNumber(10).pow(someERC20Decimals).multipliedBy(10);
+            const tokensToTransfer = new BigNumber(10)
+                .pow(someERC20Decimals).multipliedBy(defaultAmountOfTokens);
             /**
              * estimate fees to authorize transactions
              */
@@ -309,12 +442,18 @@ contract('MIXR', (accounts) => {
             await mixr.depositToken(someERC20.address, tokensToTransfer.toString(10), {
                 from: user,
             });
-        });
+        };
+
         describe('actions that should fail', () => {
+            beforeEach(beforeEachMethod);
+
             afterEach(async () => {
                 const mixrBalance = new BigNumber(await mixr.totalSupply());
-                mixrBalance.should.be.bignumber.equal(tokenNumber(mixrDecimals, 10));
+                mixrBalance.should.be.bignumber.equal(
+                    tokenNumber(mixrDecimals, defaultAmountOfTokens),
+                );
             });
+
             itShouldThrow(
                 'forbids redeeming without allowance',
                 async () => {
@@ -364,81 +503,17 @@ contract('MIXR', (accounts) => {
         });
 
         describe('actions that should work', () => {
-            it('allows to swap MIXR for something else', async () => {
-                const previousERC20Balance = new BigNumber(await someERC20.balanceOf(user));
-                const previousMixrBalance = new BigNumber(await mixr.balanceOf(user));
-                const oneToken = new BigNumber(10).pow(someERC20Decimals);
-                const oneMIXR = new BigNumber(10).pow(mixrDecimals);
+            beforeEach(beforeEachMethod);
 
-                /**
-                 * estimate fees to authorize transactions
-                 */
-                const feeInBasketWei = new BigNumber(
-                    await mixr.transactionFee(
-                        someERC20.address,
-                        oneToken.toString(10),
-                        await mixr.REDEMPTION(),
-                    ),
+            it('redeem 1 MIX by 1 token', async () => {
+                redemptionTest(
+                    1, user, walletFees, someERC20, someERC20Decimals, mixr, mixrDecimals,
                 );
-                /**
-                 * approve and deposit
-                 */
-                const amountInBasketWei = new BigNumber(
-                    await mixr.convertTokensAmount(
-                        someERC20.address,
-                        mixr.address,
-                        oneToken.toString(10),
-                    ),
-                );
-                const totalApprove = amountInBasketWei.plus(feeInBasketWei);
-                await mixr.approve(
-                    mixr.address,
-                    totalApprove.toString(10),
-                    {
-                        from: user,
-                    },
-                );
-                /**
-                 * redeem
-                 */
-                await mixr.redeemMIXR(
-                    someERC20.address,
-                    amountInBasketWei.toString(10),
-                    {
-                        from: user,
-                    },
-                );
-                /**
-                 * asserts
-                 */
-                const withoutFeeInBasketWei = amountInBasketWei.minus(feeInBasketWei);
-                const withoutFeeInTokenWei = new BigNumber(
-                    await mixr.convertTokensAmount(
-                        mixr.address,
-                        someERC20.address,
-                        withoutFeeInBasketWei.toString(10),
-                    ),
-                );
-                const feeInTokenWei = new BigNumber(
-                    await mixr.convertTokensAmount(
-                        mixr.address,
-                        someERC20.address,
-                        feeInBasketWei.toString(10),
-                    ),
-                );
-                new BigNumber(
-                    await someERC20.balanceOf(user),
-                ).should.be.bignumber.equal(
-                    previousERC20Balance.plus(withoutFeeInTokenWei),
-                );
-                new BigNumber(await mixr.balanceOf(user))
-                    .should.be.bignumber.equal(
-                        previousMixrBalance.minus(oneMIXR.plus(feeInBasketWei)),
-                    );
-                new BigNumber(
-                    await someERC20.balanceOf(mixr.address),
-                ).should.be.bignumber.equal(
-                    (new BigNumber(10).pow(someERC20Decimals).multipliedBy(9)).plus(feeInTokenWei),
+            });
+
+            it('redeem 50 MIX by 50 token', async () => {
+                redemptionTest(
+                    50, user, walletFees, someERC20, someERC20Decimals, mixr, mixrDecimals,
                 );
             });
         });
