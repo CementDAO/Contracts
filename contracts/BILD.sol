f50aac025bc01139887ba7e6129190c24794f536
Alberto Cuesta <alberto@techhq.io>
2019-02-26 14:24:49 +0000

    Changed stakes data structure to have agent as the main way of finding stakes.

--- contracts/BILD.sol
+++ contracts/BILD.sol
@@ -1,30 +1,42 @@
 pragma solidity ^0.5.0;
 
-import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 /**
  * @title BILD Staking contract. 
  * @author Alberto Cuesta Canada, Bernardo Vieira
  * @notice Implements staking of BILD tokens towards a Curation Agent Ranking
  */
-contract BILD is IERC20 {
+contract BILD is ERC20, ERC20Detailed {
     /**
      * @notice Minimum BILD wei that are accepted to nominate a new Curation Agent
      */
     uint256 minimumStake = 100000000000000000000;
 
     /**
-     * @notice A single stake of BILD from one stakeholder to one agent.
+     * @notice A single stake of BILD from one stakeholder.
      */
     struct Stake {
-        address agent;
+        address stakeholder;
         uint256 value;
     }
 
     /**
-     * @notice All BILD stakes.
+     * @notice All BILD stakes, mapped by agent address.
+     * @dev It might be more gas effective to remove this mapping and just use
+     * the agentRanking list to access the stakes.
      */
-    mapping(address => Stake[]) globalStakes;
+    mapping(address => Stake[]) stakesByAgent;
+
+    /**
+     * @notice View of aggregated stakes by stakeholder address.
+     * @dev It might be more cost efficient to traverse all stakes which each 
+     * transaction and aggregate values than to store this mapping. However, it
+     * seems more user friendly to put the cost of the platform on those that
+     * create stakes than those that transact with MIX.
+     */
+    mapping(address => uint256) stakesByHolder;
 
     /**
      * @notice One component of a linked list that contained the aggregated 
@@ -45,6 +57,28 @@ contract BILD is IERC20 {
      * @notice The address of the agent at the R position of the agentRanking.
      */
     address agentR;
+
+    /**
+     * @notice Returns an individual stake.
+     * @param _agent The agent that the stake is for.
+     * @param _stakeholder The holder that the stake is from.
+     */
+    function findStake(address _agent, address _stakeholder)
+        public
+        view
+        returns(Stake memory)
+    {
+        require (
+            stakesByAgent[_agent] != 0, 
+            "Agent not found."
+        );
+
+        for (uint256 i = 0; i <= stakesByAgent[_agent].length; i++)
+        {
+            if (stakesByAgent[_agent].stakeholder == _stakeholder)
+                return stakesByAgent[_agent];
+        }
+    }
 
     /**
      * @notice Update of the agent ranking, maintaining the linked list 
@@ -82,18 +116,18 @@ contract BILD is IERC20 {
     }
 
     /**
-     * @notice Returns the aggregation of all stakes by a stakeholder.
-     * @param _stakeholder The stakeholder to aggregate stakes for.
+     * @notice Returns the aggregation of all stakes for an agent.
+     * @param _agent The agent to aggregate stakes for.
      */
-    function staked(address _stakeholder)
+    function aggregateAgentStakes(address _agent)
         public
         view
         returns(uint256)
     {
-        Stake[] memory stakes = globalStakes[_stakeholder];
+        //Stake[] memory agentStakes = stakes[_agent];
         uint256 result = 0;
-        for (uint256 i = 0; i <= stakes.length; i++)
-            result += stakes[i].value;
+        for (uint256 i = 0; i <= stakesByAgent[_agent].length; i++)
+            result += stakesByAgent[_agent][i].value;
         return result;
     }
 
@@ -107,22 +141,25 @@ contract BILD is IERC20 {
     public
     {
         require (
-            _stake < BILD.balanceOf(msg.sender) - staked(msg.sender),
+            _stake < ERC20(address(this)).balanceOf(msg.sender) - stakesByHolder[msg.sender],
             "Attempted stake larger than BILD balance."
         );
 
         require (
-            agentRanking[_agent] > 0 || _stake >= minimumStake, 
+            agentRanking[_agent] != 0 || _stake >= minimumStake, 
             "Minimum stake not reached to nominate an agent."
         );
 
-        // Create stake
-        Stake storage stake = new Stake();
-        stake.value = _stake;
-        globalStakes[msg.sender].push(stake);
+        Stake memory stake = findStake(_agent, msg.sender);
+        // If that agent has no earlier stakes by this holder create one
+        if (stake == 0) stake = Stake(msg.sender, _stake);
+        stakesByAgent[_agent].push(stake);
         
-        // Update agent aggregate stakes
-        agentRanking[_agent].value += stake.value;
+        // Update aggregated stake views
+        if (agentRanking[_agent] == 0) 
+            agentRanking[_agent] = AggregatedStakes(0, stake.value);
+        else agentRanking[_agent].value += stake.value;
+        stakesByHolder[msg.sender] += stake.value;
 
         // If the aggregated stakes are high enough, update the agent ranking.
         if (agentRanking[_agent].value > agentRanking[agentR].value)
