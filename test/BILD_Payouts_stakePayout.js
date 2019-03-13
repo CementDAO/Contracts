4ce0b7c7451de93a6b5099e89841816fa9d9e6d9
Alberto Cuesta <alberto@techhq.io>
2019-03-13 16:29:31 +0000

    stakePayout tested.

---
+++ test/BILD_Payouts_stakePayout.js
@@ -0,0 +1,37 @@
+const BILD = artifacts.require('./BILD.sol');
+const Whitelist = artifacts.require('./Whitelist.sol');
+
+const BigNumber = require('bignumber.js');
+const chai = require('chai');
+const { itShouldThrow, tokenNumber } = require('./utils');
+// use default BigNumber
+chai.use(require('chai-bignumber')()).should();
+
+contract('BILD', (accounts) => {
+    let bild;
+    let whitelist;
+    const distributor = accounts[1];
+
+    before(async () => {
+        bild = await BILD.deployed();
+        whitelist = await Whitelist.deployed();
+    });
+
+    describe('stakePayout', () => {
+        beforeEach(async () => {
+            whitelist = await Whitelist.new();
+            bild = await BILD.new(distributor, whitelist.address);
+        });
+        it('(7*5)/3 = 4 (rounded)', async () => {
+            const totalPayout = 7;
+            const agentStakes = 5;
+            const holderStakes = 3;
+            const payout = new BigNumber(await bild.stakePayout(
+                totalPayout, 
+                agentStakes, 
+                holderStakes
+            ));
+            payout.should.be.bignumber.equal(4);
+        });
+    });
+});
