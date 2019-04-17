9cd04f16efa832751e3d1a27f7a5d9e0f282e8dc
Bernardo Vieira <bernardo5@live.com.pt>
2019-04-17 17:16:21 +0100

    jump to solidity v0.5.7

--- contracts/IMIXR.sol
+++ contracts/IMIXR.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.7;
 
 interface IMIXR {
     function getDecimals(address _token) external view returns(uint8);
--- contracts/test/SampleERC721.sol
+++ contracts/test/SampleERC721.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.7;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 
